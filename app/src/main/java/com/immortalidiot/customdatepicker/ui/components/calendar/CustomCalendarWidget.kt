b7251edc6b69e8d41c4efcb30d88fbcef5efc6e9
ImmortalIdiot <max.krasulya@yandex.ru>
2024-03-02 22:20:18 +0300

    refactor: change some parameters

--- app/src/main/java/com/immortalidiot/customdatepicker/ui/components/calendar/CustomCalendarWidget.kt
+++ app/src/main/java/com/immortalidiot/customdatepicker/ui/components/calendar/CustomCalendarWidget.kt
@@ -1,10 +1,12 @@
 package com.immortalidiot.customdatepicker.ui.components.calendar
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import java.time.YearMonth
@@ -22,14 +24,16 @@ fun CustomCalendarWidget(
     Column(
         modifier = modifier
             .fillMaxSize()
-            .padding(24.dp)
+            .padding(24.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
         Row {
             repeat(days.size) {
                 val item = days[it]
-                Item(
+                DayOfWeekItem(
                     day = item,
-                    modifier = modifier
+                    modifier = modifier.weight(1f)
                 )
             }
         }
