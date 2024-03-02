44b42e31231c238a1c103789d908d240f32a0544
ImmortalIdiot <max.krasulya@yandex.ru>
2024-03-02 03:19:11 +0300

    feat: add data class calendar

---
+++ app/src/main/java/com/immortalidiot/customdatepicker/ui/components/calendar/CalendarUiState.kt
@@ -0,0 +1,24 @@
+package com.immortalidiot.customdatepicker.ui.components.calendar
+
+import java.time.YearMonth
+
+data class CalendarUiState(
+    val yearMonth: YearMonth,
+    val dates: List<Date>
+) {
+    companion object {
+        val Init = CalendarUiState(
+            yearMonth = YearMonth.now(),
+            dates = emptyList()
+        )
+    }
+
+    data class Date(
+        val dayOfMonth: String,
+        val isSelected: Boolean
+    ) {
+        companion object {
+            val Empty = Date("", false)
+        }
+    }
+}
