a622173262734bda77a6c1d7392dbf65978e7f20
ImmortalIdiot <max.krasulya@yandex.ru>
2024-03-02 17:53:41 +0300

    feat: add date style

--- app/src/main/java/com/immortalidiot/customdatepicker/ui/theme/Type.kt
+++ app/src/main/java/com/immortalidiot/customdatepicker/ui/theme/Type.kt
@@ -59,3 +59,5 @@ val boldLato18 = boldLatoStyle.copy(fontSize = 18.sp)
 val boldLato17 = boldLatoStyle.copy(fontSize = 17.sp)
 val boldLato16 = boldLatoStyle.copy(fontSize = 16.sp)
 val boldLato12 = boldLatoStyle.copy(fontSize = 12.sp)
+
+val semiBold10 = semiBoldInterStyle.copy(fontSize = 10.sp)
