19c090305ebde0b225c492570908e71aff039074
ImmortalIdiot <max.krasulya@yandex.ru>
2024-03-02 22:13:43 +0300

    fix: change the main container from the Card to the Surface

--- app/src/main/java/com/immortalidiot/customdatepicker/ui/components/calendar/Calendar.kt
+++ app/src/main/java/com/immortalidiot/customdatepicker/ui/components/calendar/Calendar.kt
@@ -1,27 +1,25 @@
 package com.immortalidiot.customdatepicker.ui.components.calendar
 
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Card
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.tooling.preview.Preview
 import com.immortalidiot.customdatepicker.ui.viewmodels.CalendarViewModel
 
 @Composable
 fun Calendar(
-    viewModel: CalendarViewModel
+    viewModel: CalendarViewModel = CalendarViewModel()
 ) {
     val uiState by viewModel.uiState.collectAsState()
-    Card(
+    Surface(
         modifier = Modifier
             .fillMaxSize()
             .verticalScroll(rememberScrollState())
-            .padding(16.dp)
     ) {
         CustomCalendarWidget(
             days = DateStyle.daysOfWeek,
@@ -38,4 +36,10 @@ fun Calendar(
             }
         )
     }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun CalendarPreview() {
+    Calendar()
 }
