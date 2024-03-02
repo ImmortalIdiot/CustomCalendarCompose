262391728df44f4554c2888d25b1de6a1b1ef6ae
ImmortalIdiot <max.krasulya@yandex.ru>
2024-03-02 22:06:54 +0300

    refactor: change the display of the text in the header

--- app/src/main/java/com/immortalidiot/customdatepicker/ui/components/calendar/Header.kt
+++ app/src/main/java/com/immortalidiot/customdatepicker/ui/components/calendar/Header.kt
@@ -1,6 +1,9 @@
 package com.immortalidiot.customdatepicker.ui.components.calendar
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowLeft
 import androidx.compose.material.icons.filled.KeyboardArrowRight
@@ -11,7 +14,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.immortalidiot.customdatepicker.ui.theme.boldLato12
 import com.immortalidiot.customdatepicker.ui.theme.classicGray
 import java.time.YearMonth
@@ -22,7 +25,10 @@ fun Header(
     onPreviousMonth: (YearMonth) -> Unit,
     onNextMonth: (YearMonth) -> Unit,
 ) {
-    Row {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center
+    ) {
         IconButton(
             onClick = {
                 onPreviousMonth.invoke(yearMonth.minusMonths(1))
@@ -35,7 +41,7 @@ fun Header(
         }
         Text(
             modifier = Modifier
-                .weight(1f)
+                .padding(start = 10.dp, end = 10.dp)
                 .align(Alignment.CenterVertically),
             text = yearMonth.getDisplayName(),
             textAlign = TextAlign.Center,
