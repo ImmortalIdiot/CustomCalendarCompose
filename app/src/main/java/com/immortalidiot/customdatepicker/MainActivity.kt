e9e7dcbf058a47a5ff7c6aa6896d8b3fd73b10f4
ImmortalIdiot <max.krasulya@yandex.ru>
2024-08-30 08:51:53 +0300

    refactor: add a theme changer

--- app/src/main/java/com/immortalidiot/customdatepicker/MainActivity.kt
+++ app/src/main/java/com/immortalidiot/customdatepicker/MainActivity.kt
@@ -3,24 +3,32 @@ package com.immortalidiot.customdatepicker
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.ui.Modifier
+import androidx.activity.viewModels
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import com.immortalidiot.customdatepicker.ui.components.calendar.Calendar
 import com.immortalidiot.customdatepicker.ui.theme.CustomDatePickerTheme
+import com.immortalidiot.customdatepicker.ui.viewmodels.CalendarViewModel
+import com.immortalidiot.customdatepicker.ui.viewmodels.ThemeViewModel
 
 class MainActivity : ComponentActivity() {
+    private val themeViewModel: ThemeViewModel by viewModels()
+    private val calendarViewModel: CalendarViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
-            CustomDatePickerTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Calendar()
-                }
+            val currentTheme by themeViewModel.immutableState.collectAsState()
+            val darkTheme = when (currentTheme) {
+                ThemeViewModel.ThemeState.Dark -> true
+                ThemeViewModel.ThemeState.Light -> false
+                ThemeViewModel.ThemeState.System -> isSystemInDarkTheme()
+            }
+
+            CustomDatePickerTheme(darkTheme = darkTheme) {
+                Calendar(calendarViewModel = calendarViewModel)
             }
         }
     }
