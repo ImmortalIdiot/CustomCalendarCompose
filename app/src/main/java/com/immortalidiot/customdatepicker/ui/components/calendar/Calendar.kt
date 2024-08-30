b0889da44eb57d0c90b210cf87e675f78d5db8ea
ImmortalIdiot <max.krasulya@yandex.ru>
2024-08-30 08:56:45 +0300

    feat: add the select date function

--- app/src/main/java/com/immortalidiot/customdatepicker/ui/components/calendar/Calendar.kt
+++ app/src/main/java/com/immortalidiot/customdatepicker/ui/components/calendar/Calendar.kt
@@ -1,45 +1,37 @@
 package com.immortalidiot.customdatepicker.ui.components.calendar
 
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.immortalidiot.customdatepicker.ui.viewmodels.CalendarViewModel
 
 @Composable
 fun Calendar(
-    viewModel: CalendarViewModel = CalendarViewModel()
+    calendarViewModel: CalendarViewModel
 ) {
-    val uiState by viewModel.uiState.collectAsState()
-    Surface(
-        modifier = Modifier
-            .fillMaxSize()
-            .verticalScroll(rememberScrollState())
-    ) {
-        CustomCalendarWidget(
-            days = DateStyle.daysOfWeek,
-            yearMonth = uiState.yearMonth,
-            dates = uiState.dates,
-            onPreviousMonth = { previousMonth ->
-                viewModel.toPreviousMonth(previousMonth)
-            },
-            onNextMonth = { nextMonth ->
-                viewModel.toNextMonth(nextMonth)
-            },
-            onDateClickListener = {
-                // TODO: show timetable
-            }
-        )
-    }
+    val uiState by calendarViewModel.uiState.collectAsState()
+
+    CustomCalendarWidget(
+        days = DateStyle.daysOfWeek,
+        yearMonth = uiState.yearMonth,
+        dates = uiState.dates,
+        onPreviousMonth = { previousMonth ->
+            calendarViewModel.toPreviousMonth(previousMonth)
+        },
+        onNextMonth = { nextMonth ->
+            calendarViewModel.toNextMonth(nextMonth)
+        },
+        onDateClickListener = { selectedDate ->
+            calendarViewModel.selectDate(selectedDate)
+        }
+    )
 }
 
 @Preview(showSystemUi = true)
 @Composable
 fun CalendarPreview() {
-    Calendar()
+    val calendarViewModel: CalendarViewModel = viewModel()
+    Calendar(calendarViewModel = calendarViewModel)
 }
