5732b938f84e9a5d0628c7f95987d987ae101a93
ImmortalIdiot <max.krasulya@yandex.ru>
2024-08-30 08:55:25 +0300

    refactor: change theme colors

--- app/src/main/java/com/immortalidiot/customdatepicker/ui/components/calendar/CustomCalendarWidget.kt
+++ app/src/main/java/com/immortalidiot/customdatepicker/ui/components/calendar/CustomCalendarWidget.kt
@@ -1,5 +1,6 @@
 package com.immortalidiot.customdatepicker.ui.components.calendar
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -9,12 +10,12 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Divider
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.immortalidiot.customdatepicker.ui.theme.lightGray
-import com.immortalidiot.customdatepicker.ui.theme.primaryDarkBlue
+import com.immortalidiot.customdatepicker.ui.theme.ClassicGray
 import java.time.YearMonth
 
 @Composable
@@ -27,14 +28,19 @@ fun CustomCalendarWidget(
     onNextMonth: (YearMonth) -> Unit,
     onDateClickListener: (CalendarUiState.Date) -> Unit
 ) {
+    val scheme = MaterialTheme.colorScheme
+    val textColor = ClassicGray
+
     Column(
         modifier = modifier
             .fillMaxSize()
+            .background(color = scheme.onBackground)
             .padding(24.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
         Header(
+            modifier = modifier,
             yearMonth = yearMonth,
             onPreviousMonth = onPreviousMonth,
             onNextMonth = onNextMonth
@@ -42,8 +48,8 @@ fun CustomCalendarWidget(
         Column(
             modifier = modifier.border(
                 shape = RoundedCornerShape(16.dp),
-                color = primaryDarkBlue,
-                width = 4.dp
+                color = textColor,
+                width = 2.dp
             ),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center
@@ -58,11 +64,12 @@ fun CustomCalendarWidget(
                 }
             }
             Divider(
-                modifier = modifier.fillMaxWidth(0.9f),
-                color = lightGray,
+                modifier = modifier.fillMaxWidth(0.95f),
+                color = textColor,
                 thickness = 1.dp
             )
             Content(
+                modifier = modifier,
                 dates = dates,
                 onDateClickListener = onDateClickListener
             )
