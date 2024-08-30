c456acff35e7fc4aaed89cc521bc573a8ed9376f
ImmortalIdiot <max.krasulya@yandex.ru>
2024-08-30 08:54:43 +0300

    refactor: add selection date by user

--- app/src/main/java/com/immortalidiot/customdatepicker/ui/components/calendar/CalendarGrid.kt
+++ app/src/main/java/com/immortalidiot/customdatepicker/ui/components/calendar/CalendarGrid.kt
@@ -1,25 +1,34 @@
 package com.immortalidiot.customdatepicker.ui.components.calendar
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import com.immortalidiot.customdatepicker.ui.theme.boldLato12
-import com.immortalidiot.customdatepicker.ui.theme.boldLato13
-import com.immortalidiot.customdatepicker.ui.theme.darkGray
-import com.immortalidiot.customdatepicker.ui.theme.primaryDarkBlue
+import com.immortalidiot.customdatepicker.ui.theme.ClassicGray
+import com.immortalidiot.customdatepicker.ui.theme.boldLato14
+import com.immortalidiot.customdatepicker.ui.theme.mediumInter12
 
 @Composable
 fun Content(
     dates: List<CalendarUiState.Date>,
     onDateClickListener: (CalendarUiState.Date) -> Unit,
+    modifier: Modifier = Modifier,
 ) {
     Column {
         var index = 0
@@ -28,12 +37,20 @@ fun Content(
                 horizontalArrangement = Arrangement.Center
             ) {
                 repeat(7) {
-                    val item = if (index < dates.size) dates[index]
-                    else CalendarUiState.Date.Empty
+                    val item = if (index < dates.size) {
+                        dates[index]
+                    } else {
+                        CalendarUiState.Date.Empty
+                    }
+
                     ContentItem(
                         date = item,
-                        onClickListener = onDateClickListener,
-                        modifier = Modifier.weight(1f)
+                        onClickListener = if (item != CalendarUiState.Date.Empty) {
+                            onDateClickListener
+                        } else {
+                            null
+                        },
+                        modifier = modifier.weight(1f)
                     )
                     index++
                 }
@@ -45,23 +62,58 @@ fun Content(
 @Composable
 fun ContentItem(
     date: CalendarUiState.Date,
-    onClickListener: (CalendarUiState.Date) -> Unit,
+    onClickListener: ((CalendarUiState.Date) -> Unit)?,
     modifier: Modifier = Modifier
 ) {
+    val scheme = MaterialTheme.colorScheme
+
     Box(
-        modifier = modifier.clickable { onClickListener(date) },
+        modifier = modifier.clickable(
+            enabled = onClickListener != null,
+            indication = null,
+            interactionSource = remember { MutableInteractionSource() }
+        ) {
+            onClickListener?.invoke(date)
+        },
         contentAlignment = Alignment.Center
     ) {
-        Text(
-            modifier = modifier
-                .align(Alignment.Center)
-                .padding(12.dp),
-            text = date.dayOfMonth,
-            style = boldLato13,
-            color = if (date.isSelected) primaryDarkBlue
-                    else darkGray
-        )
+        if (date.isSelected) {
+            Box(
+                modifier = modifier
+                    .size(40.dp)
+                    .clip(shape = CircleShape)
+                    .background(color = scheme.primary),
+                contentAlignment = Alignment.Center
+            ) {
+                ItemText(
+                    modifier = modifier,
+                    text = date.dayOfMonth,
+                    color = scheme.primaryContainer
+                )
+            }
+        } else {
+            ItemText(
+                modifier = modifier,
+                text = date.dayOfMonth,
+                color = ClassicGray
+            )
+        }
     }
+}
+
+@Composable
+fun ItemText(
+    color: Color,
+    text: String,
+    modifier: Modifier = Modifier
+) {
+    Text(
+        modifier = modifier.padding(12.dp),
+        text = text,
+        style = boldLato14,
+        color = color,
+        textAlign = TextAlign.Center
+    )
 }
 
 @Composable
@@ -78,7 +130,7 @@ fun DayOfWeekItem(
                 .align(Alignment.Center)
                 .padding(12.dp),
             text = day.uppercase(),
-            style = boldLato12.copy(color = darkGray)
+            style = mediumInter12.copy(color = ClassicGray)
         )
     }
 }
